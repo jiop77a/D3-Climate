c5aeed3cf1c7074c431ac30df02fb8fed5d1a4a7
Vincent Chan <vincent.chan@biola.edu>
2017-12-21 22:29:25 -0800

    basic chart

--- app/test.jsx
+++ app/test.jsx
@@ -6,15 +6,14 @@ import {getAllAgriculture} from './API_calls/api_calls.js';
 class Test extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state;
     this.getKeys = this.getKeys.bind(this);
   }
 
   componentDidMount() {
     getAllAgriculture().then(data => {
-      let dats = this.getFirst(data);
+      let dats = [this.getFirst(data)];
       this.setState(dats);
-      console.log(this.state);
     })
   }
 
@@ -39,45 +38,74 @@ class Test extends React.Component {
     return res
   }
 
-  render() {
-    let data = this.data;
-    let root = new ReactFauxDOM.Element('div');
+  draw(root) {
+    let data = this.state || [[]];
+    data = data[0]
+    console.log(data)
+    let width = 800;
+    let height = 400;
+    let padding = 50;
 
     let svg = d3.select(root)
               .append('svg')
-              .attr('width', 800)
-              .attr('height', 600)
+              .attr('width', width)
+              .attr('height', height)
 
     let x_scale = d3.scaleLinear()
                   .domain([1990, 2015])
-                  .range(1990, 2015)
+                  .range([padding, width - padding * 2])
 
     let y_scale = d3.scaleLinear()
-                  .domain([d3.min(data, d => d[4]), d3.max(data, d => d[4]])
-                  .range(100, 300)
-  //
-  //   let r_scale = d3.scaleLinear()
-  //                 .domain([d3.min(data, d => d.emissionsByYear), d3.max(data, d => d.emissionsByYear)])
-  //                 .range([1,10])
-  //
-  //   let x_axis = d3.axisBottom(x_scale)
-  //               .tickFormat(d3.format("d"))
-  //               .ticks(10)
-  //
-  //
-  // svg.selectAll('circle')
-  //   .data(data)
-  //   .enter()
-  //   .append('circle')
-  //   .attr('cx', d => x_scale(d.emissionsByYear.key))
-  //   .attr('cy', d => y_scale(d.emissionsByYear.value))
-  //   .attr('r', d => r_scale(d.emissionsByYear.value))
-  //   .attr('fill', '#6ce428')
-  // {root.toReact()}
+                  .domain([
+                    d3.min(data, d => d[4]),
+                    d3.max(data, d => d[4])
+                  ])
+                  .range([height - padding, padding]);
+
+    let r_scale = d3.scaleLinear()
+                  .domain([d3.min(data, d => d[4]), d3.max(data, d => d[4])])
+                  .range([1, 10])
+
+    let x_axis = d3.axisBottom(x_scale)
+                .tickFormat(d3.format("d"))
+                .ticks(10)
+
+    svg.append('g')
+      .attr('class', 'x-axis')
+      .attr('transform', 'translate(0,' + (height - padding) + ')')
+      .call(x_axis);
+
+    let y_axis = d3.axisLeft(y_scale)
+      .ticks(5);
+
+    svg.append('g')
+      .attr('class', 'y-axis')
+      .attr('transform', 'translate(' + padding + ', 0)')
+      .call(y_axis);
+
+    svg.selectAll('circle')
+      .data(data)
+      .enter()
+      .append('circle')
+      .attr('cx', d => x_scale(d[3]))
+      .attr('cy', d => y_scale(d[4]))
+      .attr('r', d => r_scale(d[4]))
+      .attr('fill', '#6ce428')
+
+
+    return root;
+  }
+
+  render() {
+    let root = new ReactFauxDOM.Element('div');
+    // root = this.state ? this.draw(root) :
+    root = this.draw(root);
 
     return (
       <div>
-        lol
+        <div className="alex-chart">
+          {root.toReact()}
+        </div>
       </div>
     )
   }
